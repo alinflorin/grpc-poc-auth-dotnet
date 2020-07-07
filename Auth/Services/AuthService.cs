417d94d1ef2ea7ef1b3300025a509e588c2a0b76
Alin Ciu <alin.ciu@endava.com>
2020-07-08 02:19:45 +0300

    -

---
+++ Auth/Services/AuthService.cs
@@ -0,0 +1,16 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Auth.Services
+{
+    public class AuthService : Auth.AuthBase
+    {
+        public override async Task<LoginReply> Login(LoginRequest request, ServerCallContext context)
+        {
+            return await base.Login(request, context);
+        }
+    }
+}
