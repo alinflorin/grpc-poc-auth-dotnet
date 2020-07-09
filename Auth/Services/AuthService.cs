211e7211ca29caccd3203f1c62d9cee760c98383
Alin Ciu <alin.ciu@endava.com>
2020-07-09 23:38:30 +0300

    -

---
+++ Auth/Services/AuthService.cs
@@ -0,0 +1,30 @@
+﻿using Grpc.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Auth.Services
+{
+    public class AuthService : Auth.AuthBase
+    {
+        public override async Task<LoginReply> Login(LoginRequest request, ServerCallContext context)
+        {
+            var reply = new LoginReply();
+            if (request.Username.ToLower() != "admin" && request.Password.ToLower() != "grpc-poc")
+            {
+                reply.Succesful = false;
+                reply.Errors.Add("Invalid login credentials");
+                return reply;
+            }
+            reply.Succesful = true;
+            reply.User = new User { 
+                Email = "admin@admin.com",
+                FullName = "Admin Admin",
+                IsAdmin = true,
+                Username = "admin"
+            };
+            return await Task.FromResult(reply);
+        }
+    }
+}
