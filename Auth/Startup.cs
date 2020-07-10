8040fabdbaff342fd8deb0cdba13c2c5a3c71960
Alin Ciu <alin.ciu@endava.com>
2020-07-10 04:05:10 +0300

    -

--- Auth/Startup.cs
+++ Auth/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Auth.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -31,7 +32,7 @@ namespace Auth
 
             app.UseEndpoints(endpoints =>
             {
-                // endpoints.MapGrpcService<AuthService>();
+                endpoints.MapGrpcService<AuthService>();
 
                 endpoints.MapGet("/", async context =>
                 {
